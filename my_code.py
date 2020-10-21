762e22515b428cbefabb152b1bf87a0a014e98f5
ChienT1993 <21ctran@athenian.org>
2020-10-21 19:36:35 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,3 +1,15 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
-
+pets = {}
+print ("type 'none' when you want to stop")
+while True:
+    names = input("What is your pet's name?: ")
+    if names == "none":
+        break
+    types = input ("What is your pet's type (dog, cat, bird)?:")
+    if types == "none":
+        break
+    pets[names]=types
+print (pets)
+for names, types in pets.items():
+    print (names, "is a", types)
